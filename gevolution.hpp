db17c3a7eb902764b896838a715156fd4124729a
Farbod <farbod@Farbods-MacBook-Pro.local>
2018-06-19 18:45:10 +0200

    There was a bug for non-linear treatment which is solved!

--- gevolution.hpp
+++ gevolution.hpp
@@ -448,14 +448,16 @@ void projection_Tmunu_kessence( Field<FieldType> & T00, Field<FieldType> & T0i, 
           // zeta'(n) from the new values at n, zeta(n)...
           // like zeta_integer(x) (n)
           //**********************************************
+
           zeta_prime_int_n0 =
           /*Linear(1,2,3)*/      + 3. * Hcon * ( w * zeta_integer(x) + cs2 * psi ) - C2 * pi_k(x)
           /*Linear(4,5)*/        + 3. * cs2 * phi_prime + cs2 * Laplacian_pi
+          /*Non-linear terms*/   + non_linearity * (
           /*Non-linear(1,2)*/    + 2. * cs2 * phi(x) * Laplacian_pi - (1. - cs2) * psi * Laplacian_pi
           /*Non-linear(3)  */    - 3. * cs2 * Hcon * (1. + w) * pi_k(x) * Laplacian_pi
           /*Non-linear(4)  */    +(1. - cs2) * (zeta_integer(x) + psi) * Laplacian_pi
           /*Non-linear(5,6,7)*/  - cs2 * Gradphi_Gradpi + (2. * cs2 -1.) * Gradpsi_Gradpi - C3 * Gradpi_Gradpi
-          /*Non-linear(8)  */    + 2.* (1. - cs2) * GradPsiZeta_Gradpi;
+          /*Non-linear(8)  */    + 2.* (1. - cs2) * GradPsiZeta_Gradpi );
         //***********************************************************************************
         // When we get the precision and it goes out of loop we need to update zeta_integer for
         //being synched with particles and ....
