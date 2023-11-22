254ea5d2692534e4bde5a41d53f23ca5b40ed1e7
FarbodHassani <farbod.hassani@gmail.com>
2023-11-23 00:36:35 +0100

    Updated metadata
    Metadata updated to be able to request more power spectra! Also note that to compare gevolution with kevolution we need to compare deltacdm with delta (kev) when we have non cdm particles. Also delta_kess (kev) cannot be compared with deltaclass(gev) as deltaclass have radiation contribution!

--- metadata.hpp
+++ metadata.hpp
@@ -17,11 +17,11 @@
 #define GEVOLUTION_VERSION 1.2
 
 #ifndef MAX_OUTPUTS
-#define MAX_OUTPUTS 128
+#define MAX_OUTPUTS 512
 #endif
 
 #ifndef PARAM_MAX_LENGTH
-#define PARAM_MAX_LENGTH 256
+#define PARAM_MAX_LENGTH 512
 #endif
 
 #ifndef PARAM_MAX_LINESIZE
